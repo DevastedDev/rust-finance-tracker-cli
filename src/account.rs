d38c8d9be19ea7e2b423c152a52190dfc20230db
Devasted <devastedzz@gmail.com>
2025-06-05 10:22:49 +0530

    Refactored for Centralized datafile value

--- src/account.rs
+++ src/account.rs
@@ -12,7 +12,7 @@ impl Account{
             transactions: vec![]
         }
     }
-    pub fn load_transactions(&self, file_path:String) -> Result<Account,Error>{
+    pub fn load_transactions(&self, file_path:&String) -> Result<Account,Error>{
         let contents = fs::read_to_string(file_path)?;
         match serde_json::from_str::<Vec<Transaction>>(&contents){
             Ok(transactions) =>{
@@ -21,17 +21,17 @@ impl Account{
             Err(reason) => Err(Error::other(reason)),
         }
     }
-    pub fn add_transaction(&mut self, trxn: Transaction){
+    pub fn add_transaction(&mut self, trxn: Transaction,datafile:&String){
         self.transactions.push(trxn);
-        fs::write("data/data.json",serde_json::to_string_pretty(&self.transactions).unwrap()).unwrap()
+        fs::write(datafile,serde_json::to_string_pretty(&self.transactions).unwrap()).unwrap()
     }
     pub fn get_transactions(&self)-> &Vec<Transaction>{
         &self.transactions
     }
 
-    pub fn remove_transaction(&mut self,index : &usize){
+    pub fn remove_transaction(&mut self,index : &usize,datafile:&String){
         self.transactions.remove(index-1);
-        fs::write("data/data.json",serde_json::to_string_pretty(&self.transactions).unwrap()).unwrap()
+        fs::write(datafile,serde_json::to_string_pretty(&self.transactions).unwrap()).unwrap()
     }
 
 
--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,16 @@ use account::Account;
 use clearscreen;
 use std::io;
 use std::io::Write;
-
 fn main() {
-    let mut transactions = Account::init()
-        .load_transactions(String::from("data/data.json"))
-        .unwrap();
+
     utils::long_line();
+    let datafile: String = String::from("data/data.json");
+
+    // GET Stats and Initialize the account data
+    let mut transactions = Account::init().load_transactions(&datafile).unwrap();
     let stats = transactions.get_stats();
     utils::display_stats(stats.0, stats.1);
-    utils::long_line();
+
     loop {
         let mut cmd_text = String::from("");
         print!(">");
@@ -26,16 +27,16 @@ fn main() {
         let command = &input.next();
 
         match command {
-            // Application Commands
+
             Some("add") => {
                 let amount = input.next().unwrap().parse::<f64>().unwrap();
                 let description: String = input.collect::<Vec<&str>>().join(" ");
-                transactions.add_transaction(Transaction::new(amount, description))
+                transactions.add_transaction(Transaction::new(amount, description), &datafile)
             }
 
             Some("remove") => {
                 let id = &input.next().unwrap().parse::<usize>().unwrap();
-                transactions.remove_transaction(id);
+                transactions.remove_transaction(id, &datafile);
                 println!("Removed ID: {}", id)
             }
             Some("list") => {
@@ -44,13 +45,13 @@ fn main() {
                     println!("{:#?}. {} Spent For {}", i + 1, el.amount, el.description)
                 }
             }
-            Some("stats") =>{
+            Some("stats") => {
                 utils::long_line();
                 let stats = transactions.get_stats();
                 utils::display_stats(stats.0, stats.1);
                 utils::long_line();
             }
-            // Normal Commands
+
             Some("clear") => {
                 clearscreen::clear().expect("failed to clear screen");
             }
@@ -58,7 +59,7 @@ fn main() {
                 println!("Quitting");
                 break;
             }
-            // Fallbacks`
+
             Some(_) => {
                 println!("No Command Exists");
             }
